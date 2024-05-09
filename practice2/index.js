d90e9441ea21fe95cfe730f2d7cee88cea894da3
dullat <dullat@users.noreply.github.com>
2024-05-09 08:11:36 +0530

    update

--- practice2/index.js
+++ practice2/index.js
@@ -13,11 +13,33 @@ function setError(element, message) {
   errorDiv.textContent = message
 }
 
+function validateEmailOrUsername(input) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/
+
+  return emailRegex.test(input) || usernameRegex.test(input)
+}
+
+function validatePassword(input) {
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/
+
+  return passwordRegex.test(input)
+}
+
 const validateInputs = () => {
   let uName = userName.value.trim()
   let uPass = password.value.trim()
 
   if (uName === "") {
     setError(userName, "name is require")
+  } else if (!validateEmailOrUsername(uName)) {
+    setError(userName, "enter valid user or email")
+  }
+
+  if (uPass === "") {
+    setError(userName, "pass is require")
+  } else if (!validatePassword(uPass)) {
+    setError(uPass, "enter valid passeord format")
   }
 }
--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const inputs = document.querySelectorAll('[enter]');
-const labels = document.querySelectorAll('label');
-const btn = document.querySelector('[type="submit"]');
+const inputs = document.querySelectorAll("[enter]")
+const labels = document.querySelectorAll("label")
+const btn = document.querySelector('[type="submit"]')
 
 inputs.forEach((input, index) => {
-    input.addEventListener('input', () => {
-        if (input.value !== "") {
-            labels[index].setAttribute('up', 'yes');
-            console.log(labels[index]);
-        } else if (input.value === "") {
-            labels[index].removeAttribute('up');
-        }
+  input.addEventListener("input", () => {
+    if (input.value !== "") {
+      labels[index].setAttribute("up", "yes")
+      console.log(labels[index])
+    } else if (input.value === "") {
+      labels[index].removeAttribute("up")
+    }
 
-        if (!input.validity.valid) {
-            input.classList.add('invalid');
-            labels[index].classList.add('invalid');
-            btn.style.cssText = 'background:rgba(0, 128, 0, 0.582); cursor: not-allowed;';
-        } else {
-            input.classList.remove('invalid');
-            labels[index].classList.remove('invalid');
-            btn.style.cssText = 'background: green; cursor: pointer;';
-        }
-    });
-});
+    if (!input.validity.valid) {
+      input.classList.add("invalid")
+      labels[index].classList.add("invalid")
+      btn.style.cssText =
+        "background:rgba(0, 128, 0, 0.582); cursor: not-allowed;"
+    } else {
+      input.classList.remove("invalid")
+      labels[index].classList.remove("invalid")
+      btn.style.cssText = "background: green; cursor: pointer;"
+    }
+  })
+})
